94bbcf5dcde78562700d650c7baf9b81d2e81ea9
dedechndra <dedech888@gmail.com>
2023-07-21 00:28:36 +0700

    create int main Jumlah_mahasiswa

--- exercise !.cpp
+++ exercise !.cpp
@@ -9,3 +9,27 @@ struct Mahasiswa {
     int nilaiBahasaInggris;
     bool diterima;
 };
+int main() {
+    const int JUMLAH_MAHASISWA = 20;
+
+    Mahasiswa mahasiswa[JUMLAH_MAHASISWA];
+
+    // Input nilai mahasiswa
+    for (int i = 0; i < JUMLAH_MAHASISWA; i++) {
+        cout << "Masukkan nama mahasiswa ke-" << i + 1 << ": ";
+        cin >> mahasiswa[i].nama;
+
+        cout << "Masukkan nilai matematika mahasiswa ke-" << i + 1 << ": ";
+        cin >> mahasiswa[i].nilaiMatematika;
+
+        cout << "Masukkan nilai bahasa Inggris mahasiswa ke-" << i + 1 << ": ";
+        cin >> mahasiswa[i].nilaiBahasaInggris;
+
+        // Cek apakah diterima
+        if ((mahasiswa[i].nilaiMatematika + mahasiswa[i].nilaiBahasaInggris) / 2 >= 70 || mahasiswa[i].nilaiMatematika > 80) {
+            mahasiswa[i].diterima = true;
+        }
+        else {
+            mahasiswa[i].diterima = false;
+        }
+    }
